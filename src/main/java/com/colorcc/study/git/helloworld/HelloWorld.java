3ced0539b0d4b08b133beefb6a5a32fe3c918480
jackqin <followfollowme@163.com>
2010-12-24 00:47:31 +0800

    Signed-off-by: jackqin <followfollowme@163.com>
    Use log for the project.

--- src/main/java/com/colorcc/study/git/helloworld/HelloWorld.java
+++ src/main/java/com/colorcc/study/git/helloworld/HelloWorld.java
@@ -1,14 +1,20 @@
 package com.colorcc.study.git.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class HelloWorld {
+	
+	private final static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		logger.info("use log for hello world project!");
 		System.out.println("Hello World!");
-
 	}
 
 }
