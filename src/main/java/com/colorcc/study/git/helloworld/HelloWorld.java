ba7e00be4b838a0b89925b6ce098ab541e284d16
jackqin <followfollowme@163.com>
2010-12-24 00:29:43 +0800

    Signed-off-by: jackqin <followfollowme@163.com>
    Create hello world project.

---
+++ src/main/java/com/colorcc/study/git/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.colorcc.study.git.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
